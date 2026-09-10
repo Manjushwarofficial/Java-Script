8d5c0ee75f8500db4370403f3e01161ff0e19895
Manjushwarofficiale <manjushwarofficial@gmail.com>
2026-09-10 22:50:19 +0530

    Initial commit

---
+++ data_types.js
@@ -0,0 +1,22 @@
+/* data types: 
+ * 1. String
+ * 2. Number
+ * 3. Boolean
+ * 4. Undefined
+ * 5. Null
+ * 6. Object
+ * 7. Symbol
+ */
+
+q = 4/0;
+console.log(q/Infinity); // Infinity
+
+console.log(typeof q);
+
+
+
+
+
+
+
+
